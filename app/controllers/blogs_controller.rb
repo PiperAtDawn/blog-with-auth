8b2be172ec1ea07efaef8ea41f8c0569e495316e
PiperAtDawn <master_zayats@mail.ru>
2022-03-31 18:10:40 +0300

    Initial commit

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,54 @@
+class BlogsController < ApplicationController
+
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+
+    if @blog.save
+      redirect_to @blog
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+
+    @blog.destroy
+
+    redirect_to :action => :index
+  end
+
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+
+    if @blog.update(blog_params)
+      redirect_to @blog
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
